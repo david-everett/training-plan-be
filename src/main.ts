2b0f909db9a97d4fc67ed23810329dea324b41c2
David Everett <dwe.everett@gmail.com>
2024-05-06 19:06:28 -0400

    port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(4000);
+  const port = process.env.PORT || 4000;
+  app.listen(port, () => {});
 }
 bootstrap();
